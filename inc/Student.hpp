bd6fefd88664925222c2e75c9060c7b4cfe5b44d
Spror <wiktorspringer27@gmail.com>
2023-05-21 18:50:48 +0200

    creating files structure

---
+++ inc/Student.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+struct adress{
+
+    std::string city;
+    std::string street;
+    std::string postCode;
+    std::string number;
+};
+
+class Student{
+
+    std::string name_;
+    std::string lastName_;
+
+
+public:
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Student.hpp>
+
+
+int main(){
+
+Student student_1;
+
+std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
