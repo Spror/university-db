4223cbb41f5d1b6fff8b4ef5ec4058ba6db6129a
Spror <wiktorspringer27@gmail.com>
2023-05-25 23:12:53 +0200

    adding GoogleTest to CMakeLists.txt

--- test/test.cpp
+++ test/test.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
 
 int main(int argc, char **argv){
 
